61e4b146708ab7dbfca8c0b4f578bb6d04020735
Silloky <ek.mindstorms@outlook.com>
2022-10-29 15:05:50 +0200

    modified

--- KS-Deploy.ps1
+++ KS-Deploy.ps1
@@ -75,6 +75,39 @@
 ##OcrLFtDXTiW5
 ##LM/BD5WYTiiZ4tI=
 ##McvWDJ+OTiiZ4tI=
+##OMvOC56PFnzN8u+Vs1Q=
+##M9jHFoeYB2Hc8u+Vs1Q=
+##PdrWFpmIG2HcofKIo2QX
+##OMfRFJyLFzWE8uK1
+##KsfMAp/KUzWJ0g==
+##OsfOAYaPHGbQvbyVvnQX
+##LNzNAIWJGmPcoKHc7Do3uAuO
+##LNzNAIWJGnvYv7eVvnQX
+##M9zLA5mED3nfu77Q7TV64AuzAgg=
+##NcDWAYKED3nfu77Q7TV64AuzAgg=
+##OMvRB4KDHmHQvbyVvnQX
+##P8HPFJGEFzWE8tI=
+##KNzDAJWHD2fS8u+Vgw==
+##P8HSHYKDCX3N8u+Vgw==
+##LNzLEpGeC3fMu77Ro2k3hQ==
+##L97HB5mLAnfMu77Ro2k3hQ==
+##P8HPCZWEGmaZ7/K1
+##L8/UAdDXTlGDjpHa5z154nfKR3gnYdynkoXq4ISs8+X19SDBTPo=
+##Kc/BRM3KXxU=
+##
+##
+##fd6a9f26a06ea3bc99616d4851b372ba
+##[Ps1 To Exe]
+##
+##Kd3HDZOFADWE8uK1
+##Nc3NCtDXThU=
+##Kd3HFJGZHWLWoLaVvnQnhQ==
+##LM/RF4eFHHGZ7/K1
+##K8rLFtDXTiW5
+##OsHQCZGeTiiZ4NI=
+##OcrLFtDXTiW5
+##LM/BD5WYTiiZ4tI=
+##McvWDJ+OTiiZ4tI=
 ##OMvOC56PFnzN8u+VslQ=
 ##M9jHFoeYB2Hc8u+VslQ=
 ##PdrWFpmIG2HcofKIo2QX
@@ -456,7 +489,7 @@ if ($new -eq $true){
         $userDataDir = "$env:appdata\Kirkwood Soft"
     } elseif ($installLocation -eq "U"){
         $binairiesDir = "$env:appdata\Kirkwood Soft\binairies"
-        $pathname = "kirkwood Soft"
+        $pathname = "Kirkwood Soft"
         $userDataDir = "$env:appdata\Kirkwood Soft\data"
     }
     creatingLoading -createType "directory" -createpath "$binairiesDir" -createname "$pathname" -lang "$lang"
@@ -560,7 +593,7 @@ if ($new -eq $true){
             Write-Host -NoNewline "`r[ ] Configuring scheduled task action..."
             $timesofpoint = $timesofpoint + 1
         } until ($timesofpoint -eq 2)
-        $scheduledAction = New-ScheduledTaskAction -Execute 'pwsh.exe' -Argument "-File `"$binairiesDir\updateChecker.ps1e`""
+        $scheduledAction = New-ScheduledTaskAction -Execute 'pwsh.exe' -Argument "-File `"$binairiesDir\updateChecker.ps1`""
         Write-Host "`r[✓] Configuring scheduled task action... Done !"
         $timesofpoint = 0
         do {
